7736408917d89d4fd2e77939cce314a2891124f3
Yusof Sadat Fakhr <yusofsadat@gmail.com>
2025-09-24 21:45:35 +0330

    create admin user and patient user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\AdminStatus;
+use App\Models\Admin;
+use App\Models\Patient;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Enums\UserRole;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,14 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $adminUser = User::factory()->create([
+            'role' => UserRole::ADMINISTRATOR->value,
+            'email' => 'admin@example.com',
+            'user_name' => 'admin',
+            'password' => bcrypt('123456'),
+        ]);
 
-        User::factory()->create([
-            'user_name' => 'testuser',
-            'first_name' => 'Test',
-            'last_name' => 'User',
-            'email' => 'test@example.com',
-            'role' => 'بیمار',
+        Admin::factory()->create([
+            'user_id' => $adminUser->id,
+            'status' => AdminStatus::ACTIVE->value,
+            'display_name' => 'Super Admin',
+        ]);
+
+        $patientUser = User::factory()->create([
+            'role' => UserRole::PATIENT->value,
+            'email' => 'patient@example.com',
+            'user_name' => 'patient',
+            'password' => bcrypt('123456')
+        ]);
+
+        Patient::factory()->create([
+            'user_id' => $patientUser->id,
         ]);
     }
 }
