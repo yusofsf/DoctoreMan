fc7107f29329d200bb161b60ccfdd823362a5acf
Yusof Sadat Fakhr <yusofsadat@gmail.com>
2025-11-08 20:32:04 +0330

    delete policy Admin.php and create AdminPolicy.php

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use App\Enums\AdminStatus;
-use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
---
+++ app/Policies/AdminPolicy.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AdminPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user): bool
+    {
+        return $user->isAdministrator();
+    }
+
+    /**
+     * Determine whether the user can store models.
+     */
+    public function store(User $user): bool
+    {
+        return $user->isAdministrator();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdministrator();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user): bool
+    {
+        return $user->isAdministrator();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user): bool
+    {
+        return $user->isAdministrator();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(): bool
+    {
+        return false;
+    }
+}
