505ab03d44aaf74ab586d4202a9a78fcb847dbfb
Yusof Sadat Fakhr <yusofsadat@gmail.com>
2025-11-08 20:32:54 +0330

    create policies for models

---
+++ app/Policies/PatientPolicy.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Patient;
+use App\Models\User;
+
+class PatientPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Patient $patient): bool
+    {
+        if ($user->isAdministrator()) {
+            return true;
+        }
+        if ($user->id === $patient->user_id) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can store models.
+     */
+    public function store(User $user): bool
+    {
+        return $user->isPatient();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isPatient();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Patient $patient): bool
+    {
+        if ($user->isAdministrator()) {
+            return true;
+        }
+        if ($user->id === $patient->user_id) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Patient $patient): bool
+    {
+        if ($user->isAdministrator()) {
+            return true;
+        }
+        if ($user->id === $patient->user_id) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Patient $patient): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Patient $patient): bool
+    {
+        return false;
+    }
+}
